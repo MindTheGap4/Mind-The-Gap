2884be3263e29b4a2eea2f1c38fe164f00ff9458
marisaoppenheim <marisa.oppenheim@yahoo.com>
2018-06-25 19:42:56 -0400

    basicSetUp

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -7,7 +7,26 @@ import Piechart from './Points/PieChart'
  * COMPONENT
  */
 export const UserHome = props => {
-  const {firstName, lastName} = props
+  const {firstName, lastName, activities, points} = props
+  let repPoints = 0
+  let donationPoints = 0
+  const currentPoints = points && points.currentPoints.goal
+  const thisMonth =
+    activities &&
+    activities.filter(
+      activity =>
+        new Date(activity.date).getMonth() === new Date().getMonth() &&
+        new Date(activity.date).getFullYear() === new Date().getFullYear()
+    )
+  thisMonth.forEach(activity => {
+    if (activity.category === 'contact representative') {
+      repPoints += activity.points
+    }
+    if (activity.category === 'donation') {
+      donationPoints += activity.points
+    }
+  })
+
   return (
     <div>
       <h3>
@@ -20,8 +39,9 @@ export const UserHome = props => {
           outerRadius={200} /* controls how big it renders */
           innerRadius={100}
           data={[
-            {value: 92 - 34, label: 'Code lines'},
-            {value: 34, label: 'Empty lines'}
+            {value: repPoints, label: 'Contacted Rep'},
+            {value: donationPoints, label: 'Donation'},
+            {value: currentPoints - donationPoints - repPoints, label: 'To Go'}
           ]}
         />
       </div>
@@ -36,9 +56,11 @@ const mapState = state => {
     email: state.user.email,
     points: state.points,
     firstName: state.user.firstName,
-    lastName: state.user.lastName
+    lastName: state.user.lastName,
+    activities: state.activities.activityList
   }
 }
+
 export default connect(mapState)(UserHome)
 /**
  * PROP TYPES

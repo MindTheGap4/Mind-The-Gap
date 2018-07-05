fcc82c477e57fe7a7cfd4a55dfed0f207a86f551
marisaoppenheim <marisa.oppenheim@yahoo.com>
2018-07-05 12:26:25 -0400

    repStyle

--- client/components/Representatives/CongressChoice.js
+++ client/components/Representatives/CongressChoice.js
@@ -12,8 +12,7 @@ import Grid from '@material-ui/core/Grid'
 
 const styles = theme => ({
   root: {
-    flexGrow: 2,
-    marginTop: '13%'
+    flexGrow: 2
   },
   paper: {
     padding: theme.spacing.unit * 2,

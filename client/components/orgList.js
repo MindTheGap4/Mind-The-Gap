26de0794d720ba17f959b793cc13bbf34ab027aa
marisaoppenheim <marisa.oppenheim@yahoo.com>
2018-07-05 15:25:53 -0400

    dont tuch the grid

--- client/components/orgList.js
+++ client/components/orgList.js
@@ -8,8 +8,17 @@ import Grid from '@material-ui/core/Grid'
 import Textfield from './textfield'
 
 const styles = theme => ({
+  root: {
+    flexGrow: 2
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  },
   card: {
-    minWidth: 325
+    width: 325,
+    height: 350
   },
   title: {
     marginBottom: 16,
@@ -34,46 +43,54 @@ const OrgList = props => {
 
   if (props.results) {
     return (
-      <Grid
-        container
-        className={classes.card}
-        spacing={16}
-        justify="center"
-        style={{padding: 50}}
-      >
-        {props.results.map(org => (
-          <Grid key={org.url} item xs={3}>
-            <Grid
-              container
-              className={classes.demo}
-              justify="center"
-              spacing={16}
-              style={{padding: 20}}
-            >
-              <Card className={classes.card}>
-                <CardContent>
-                  <Typography variant="headline" align="center" component="h1">
-                    <a href={org.url} color="textSecondary">{org.charityName}</a>
-                  </Typography>
-                  <Typography variant="subheading" align="center">
-                    {org.city}
-                  </Typography>
-                  <Typography variant="subheading" align="center">
-                    {org.state}
-                  </Typography>
-                  <Typography variant="body2" align="center">
-                    <a href={org.donationUrl}>Donate Here</a>
-                  </Typography>
-                  <Textfield
-                    orgName={org.charityName}
-                    donationUrl={org.donationUrl}
-                  />
-                </CardContent>
-              </Card>
+      <div className={classes.root}>
+        <Grid
+          container
+          className={classes.paper}
+          spacing={16}
+          justify="space-between"
+          style={{padding: 50}}
+        >
+          {props.results.map(org => (
+            <Grid key={org.url} item xs={3}>
+              <Grid
+                container
+                className={classes.demo}
+                justify="space-between"
+                spacing={16}
+                style={{padding: 20}}
+              >
+                <Card className={classes.card}>
+                  <CardContent>
+                    <Typography
+                      variant="headline"
+                      align="center"
+                      component="h1"
+                    >
+                      <a href={org.url} color="textSecondary">
+                        {org.charityName}
+                      </a>
+                    </Typography>
+                    <Typography variant="subheading" align="center">
+                      {org.city}
+                    </Typography>
+                    <Typography variant="subheading" align="center">
+                      {org.state}
+                    </Typography>
+                    <Typography variant="body2" align="center">
+                      <a href={org.donationUrl}>Donate Here</a>
+                    </Typography>
+                    <Textfield
+                      orgName={org.charityName}
+                      donationUrl={org.donationUrl}
+                    />
+                  </CardContent>
+                </Card>
+              </Grid>
             </Grid>
-          </Grid>
-        ))}
-      </Grid>
+          ))}
+        </Grid>
+      </div>
     )
   } else {
     return null

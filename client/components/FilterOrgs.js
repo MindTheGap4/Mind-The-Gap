b2fc28e08eddbc4df629e9d3ccea4292e511a29c
Crystal Mora <ChibiMora@users.noreply.github.com>
2018-07-05 00:57:35 -0400

    started styling the filter orgs

--- client/components/FilterOrgs.js
+++ client/components/FilterOrgs.js
@@ -19,6 +19,9 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 3
   },
   textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
     flexBasis: 200
   }
 })
